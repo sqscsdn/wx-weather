05a07e10debf3fe3fa15165cd66942004a22c17a
sqscsdn <89011683+sqscsdn@users.noreply.github.com>
2023-07-19 17:43:51 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
   appid: "wx78e52972ee75d2ad",
-  appsecret: "85f9fadb24272645f3b1509e86313268",
+  appsecret: "dde58f95a3936eefe5387e2e82b4962a",
   template_id: "mYLcCrhHsGH_0j2RwGfpqQB1rr-y7o76Im9F-D_5kR4",
   touser: "oMd2x6tZ2rQwVg47ex0wN5YCrSjU",
 
-  key: "201c93c2e2b34bdeecdbf9f5265eb84e",
+  key: "5567d63cecb14b53bcdf793a29989418",
   city: "双流区",
   first_date: "2022-01-01",
 };
