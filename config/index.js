670f87ed949f1c83d0b2af62d7b4ea237ed90538
sqscsdn <89011683+sqscsdn@users.noreply.github.com>
2023-07-19 17:48:38 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   appid: "wx78e52972ee75d2ad",
   appsecret: "dde58f95a3936eefe5387e2e82b4962a",
-  template_id: "mYLcCrhHsGH_0j2RwGfpqQB1rr-y7o76Im9F-D_5kR4",
+  template_id: "QODTpqJlmgs0OojeEHnhHCSni8VgWziTFy-1CWVWQTA",
   touser: "oMd2x6tZ2rQwVg47ex0wN5YCrSjU",
 
   key: "5567d63cecb14b53bcdf793a29989418",
